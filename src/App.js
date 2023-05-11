edb876d38a14b5f15884f17ecd5ff80f5ca75712
UWAJENEZANadine <adelineu640@gmail.com>
2023-05-11 15:36:01 +0200

    whole project stylesheet

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Header from "./components/header";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Footer from "./components/footer";
+import GoToTop from "./components/GoToTop";
 
 function App() {
   return (
     <>
       <Header />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
       </Routes>
+      <GoToTop />
       <Footer />
     </>
   );
