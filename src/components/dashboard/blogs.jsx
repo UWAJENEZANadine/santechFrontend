cf1cb608491dca17a977b6e3f92f2671e270da1e
UWAJENEZANadine <adelineu640@gmail.com>
2023-06-06 11:22:28 +0200

    dash tech

--- src/components/dashboard/blogs.jsx
+++ src/components/dashboard/blogs.jsx
@@ -1,82 +1,68 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import "../../css/LayoutDash.css";
 import "../../css/techBlog.css";
 
 const Blogs = () => {
-  const [fname, setFname] = useState("");
-  const [image, setImage] = useState("");
+  const [value, setValue] = useState("");
+  console.log(value);
   return (
-    <div className="main-content form-container-part">
-      <div className="container mt-3 py-5 form-container">
-      
-        <h1>New blogs<span> post Entery</span></h1>
-        <Form className="card px-5 py-5 form-party" >
-          <Form.Group className="mb-5">
-            <Form.Control
-              type="text"
-              placeholder="Blog title"
-              name="fname"
-              onChange={(event) => {
-                setFname(event.target.value);
-              }}
-              style={{ height: "6vh", fontSize: "1.5rem" }}
+    <div className="main-content">
+      <div className="add">
+        <div className="content-blog">
+          <input type="text" placeholder="Title" />
+          <div className="editor-container">
+            <ReactQuill
+              theme="snow"
+              className="editor"
+              value={value}
+              onChange={setValue}
             />
-          </Form.Group>
-          <Form.Group className="mb-5">
-            <textarea
-              className="form-control"
-              placeholder="description"
-              rows="5"
-              style={{fontSize: "1.5rem" }}
-            />
-          </Form.Group>
-          <Form.Group className="mb-5">
-            <Form.Control
-              type="date"
-              id="start"
-              min="2000-01-01"
-              max="2023-12-31"
-              name="fname"
-              onChange={(event) => {
-                setFname(event.target.value);
-              }}
-              style={{ height: "6vh", fontSize: "1.5rem" }}
-            />
-          </Form.Group>
-          <Form.Group className="mb-5">
-            <Form.Control
-              type="time"
-              id="appt"
-              min="09:00"
-              max="18:00"
-              onChange={(event) => {
-                setFname(event.target.value);
-              }}
-              style={{ height: "6vh", fontSize: "1.5rem" }}
-            />
-          </Form.Group>
+          </div>
+        </div>
+        <div className="menu-blog">
+          <div className="item-blog">
+            <h1>Publish</h1>
+            <span style={{ fontSize: "1.3rem" }}>
+              <b>Status:</b> Draft
+            </span>
+            <span style={{ fontSize: "1.3rem" }}>
+              <b>Visibility:</b> Public
+            </span>
+            <input type="file" name="" id="file" style={{ display: "none" }} />
+            <label htmlFor="file" className="file">
+              Upload image
+            </label>
+            <div className="buttons">
+              <button>Save as draft</button>
+              <button>Update</button>
+            </div>
+          </div>
+          <div className="item-blog">
+            <h1>Category</h1>
+            <div className="blog-radio">
+              <input type="radio" name="inspiration" value="art" id="events" />
 
-          <Form.Group className="mb-5">
-            <Form.Control
-              type="file"
-              placeholder="Boh image"
-              name="photo"
-              onChange={(event) => {
-                setImage(event.target.files[0]);
-              }}
-              style={{ height: "6vh", fontSize: "1.6rem" }}
-            />
-          </Form.Group>
-
-          <Button variant="primary" type="submit" style={{ width: "40%" }}>
-            upload
-          </Button>
-        </Form>
+              <label htmlFor="events">Events</label>
+            </div>
+            <div className="blog-radio">
+              <input type="radio" name="inspiration" value="art" id="news" />
+              <label htmlFor="news">News</label>
+            </div>
+            <div className="blog-radio">
+              <input
+                type="radio"
+                name="inspiration"
+                value="art"
+                id="resources"
+              />
+              <label htmlFor="resources">Resources</label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
- 
   );
 };
 
